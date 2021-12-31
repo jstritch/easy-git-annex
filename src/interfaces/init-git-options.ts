fd3183fcd76cfa7b3ec2d591326d760c1b1f9ccb
John Stritch <john.stritch@icloud.com>
2021-12-31 15:20:27 -0600

    update initGit options

--- src/interfaces/init-git-options.ts
+++ src/interfaces/init-git-options.ts
@@ -5,27 +5,7 @@
 export interface InitGitOptions {
 
   /**
-   * Create a bare repository.
+   * Creates a bare repository.
    */
   '--bare'?: null;
-
-  /**
-   * Use the specified name for the initial branch in the newly created repository.
-   */
-  '--initial-branch'?: string;
-
-  /**
-   * Specify that the Git repository is to be shared amongst several users.
-   */
-  '--shared'?: string;
-
-  /**
-   * Specify the directory from which templates will be used.
-   */
-  '--template'?: string;
-
-  /**
-   * Only print error and warning messages; all other output will be suppressed.
-   */
-  '--quiet'?: null;
 }
---
+++ tests/git-commands/init-git.test.ts
@@ -0,0 +1,29 @@
+import * as anx from '../../src/index';
+import { createDirectory, deleteDirectory } from '../helpers';
+
+describe('init', () => {
+  let repositoryPath: string;
+
+  beforeEach(async () => {
+    repositoryPath = await createDirectory();
+  });
+
+  afterEach(async () => {
+    await deleteDirectory(repositoryPath);
+  });
+
+  test('correctly initializes a git repository', async () => {
+    const myAnx = anx.createAccessor(repositoryPath);
+    const result = await myAnx.initGit();
+
+    expect(result.exitCode).toBe(0);
+  });
+
+  test('correctly initializes a bare git repository', async () => {
+    const myAnx = anx.createAccessor(repositoryPath);
+    const result = await myAnx.initGit({ '--bare': null });
+
+    expect(result.exitCode).toBe(0);
+  });
+
+});
