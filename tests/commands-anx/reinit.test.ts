ea96d54084641d38a364ccec1af82d88580935af
jstritch <john.stritch@icloud.com>
2023-05-04 10:29:02 -0500

    improve tests

--- tests/commands-anx/reinit.test.ts
+++ tests/commands-anx/reinit.test.ts
@@ -17,6 +17,7 @@ describe('reinit', () => {
   test('reinitializes a git repository', async () => {
     const uuid = '73c6cc5e-4d37-11ec-b03c-4728df437afb';
     const rslt = await myAnx.reinit(uuid);
+    expect(rslt).toBeNull();
     expect(rslt.exitCode).toBe(0);
 
     const repositoryInfos = await myAnx.getRepositories();
