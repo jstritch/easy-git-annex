b50f0dc0dde84e978fe75f6dd7c0fbcb081ffd26
jstritch <john.stritch@icloud.com>
2023-06-02 11:06:58 -0500

    add devcontainer with git-annex

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,9 @@
+echo Downloading git-annex
+curl --max-time 120 --silent --output git-annex-standalone-amd64.tar.gz https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-amd64.tar.gz
+
+echo Installing git-annex
+mkdir -p "$(systemd-path user-binaries)"
+tar --extract --gzip --strip-components 1 --file=git-annex-standalone-amd64.tar.gz --directory "$(systemd-path user-binaries)"
+
+echo Removing download
+rm git-annex-standalone-amd64.tar.gz
